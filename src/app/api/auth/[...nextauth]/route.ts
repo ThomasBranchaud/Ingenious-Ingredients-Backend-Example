7233fa08374bc61d40095f86515f1a232e8f95a0
ThomasBranchaud <156246049+ThomasBranchaud@users.noreply.github.com>
2025-03-12 16:25:36 -0400

    Delete src/app directory

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import * as dotenv from "dotenv";
-import {authOptions} from "@/app/authOptions";
-
-dotenv.config();
-
-const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST};
--- src/app/recipes/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <main>
-            <h1>Recipe not found</h1>
-            <p>Unfortunately, we could not find the requested page or recipe.</p>
-        </main>
-    );
-}
